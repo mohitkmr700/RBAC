97e5f2da9af330a24fc5fced1da02e00378d94c9
mohitkmr700 <mohitkmr700@users.noreply.github.com>
2025-04-14 00:11:43 +0530

    added health endpoint to check api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [AuthModule], // Import the AuthModule
+  imports: [AuthModule,HealthModule], // Import the AuthModule
   controllers: [],
   providers: [],
 })
