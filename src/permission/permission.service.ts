04fb444795d043d02c84abd8267f00aaa93ae014
mohitkmr700 <mohitkmr700@users.noreply.github.com>
2025-06-26 01:39:40 +0530

    Update PermissionService to format current date and time: modified timestamp generation to include milliseconds and changed format to a more readable string representation.

--- src/permission/permission.service.ts
+++ src/permission/permission.service.ts
@@ -34,7 +34,8 @@ export class PermissionService {
       const hours = String(now.getHours()).padStart(2, '0');
       const minutes = String(now.getMinutes()).padStart(2, '0');
       const seconds = String(now.getSeconds()).padStart(2, '0');
-      const currentDateTime = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.000Z`;
+      const milliseconds = String(now.getMilliseconds()).padStart(3, '0');
+      const currentDateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}Z`;
 
       const data = {
         email: createPermissionDto.email,
